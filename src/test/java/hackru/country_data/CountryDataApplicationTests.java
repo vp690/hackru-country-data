b2498ddcb8a69f8a5d349628f421b799258f7cec
vp690 <vp690@scarletmail.rutgers.edu>
2025-10-04 15:08:46 -0400

    initalize spring api

---
+++ src/test/java/hackru/country_data/CountryDataApplicationTests.java
@@ -0,0 +1,13 @@
+package hackru.country_data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountryDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
